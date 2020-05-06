5ad4ffe395b604b512f7a43cfb2a952240431a27
J. Cohen <hostelmarketing@gmail.com>
2020-05-06 01:47:25 -0700

    Add code and notes

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,35 @@ pub struct MessageApp {
     port: u16,
 }
 
+#[derive(Serialize)]
+struct IndexResponse {
+    message: String,
+}
+
+// `Result` was explicitly imported from `actix_web`, which defines its error type.
+#[get("/")]
+fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
+    let hello = req
+        .headers()
+        // a header: `hello: something` as `Option<&HeaderValue>`
+        .get("hello")
+        // If there is a value, call closure with the value. `to_str` on
+        // `&HeaderValue` returns `Result<&str, ToStrError>`. `.ok()`
+        // turns an success value from a `Result` into an `Ok` of an
+        // `Option`, but if the `Result` has an error, it turns it into
+        // a `None`.
+        .and_then(|v| v.to_str().ok())
+        // It's taking an `Option` here and unwrapping it. If it's a
+        // `None`, it will run the function, which returns an `&str`.
+        .unwrap_or_else(|| "world");
+
+    Ok(web::Json(IndexResponse {
+        // You can convert the &str to String with `to_owned`,
+        // `to_string`, or `into`.
+        message: hello.to_owned(),
+    }))
+}
+
 // A type can have multiple impl blocks, but there is typically only one
 // main one.
 impl MessageApp {
@@ -30,11 +59,17 @@ impl MessageApp {
     pub fn run(&self) -> std::io::Result<()> {
         println!("starting http server on 127.0.0.1:{}", self.port);
 
+        // This is a closure with no arguments. The `move` keyword moves
+        // the variables into the closure, otherwise references are
+        // passed in. It's here to indicate that the closure is entirely
+        // owned by the `HttpServer`.
         HttpServer::new(move || {
-            App::new()
+            App::new() // a collection of routes and their handlers
                 .wrap(middleware::Logger::default())
                 .service(index)
         })
+        // The `?` means that if the returned `Result` is `Err`, return
+        // early with the unwrapped `Err`(?)
         .bind(("127.0.0.1", self.port))?
         .workers(8)
         .run()
